80acfc552ad4ea8ddac601e7aee183c69369f44b
Hari03750 <hariharan2004b@gmail.com>
2025-05-30 12:19:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('themeToggle').addEventListener('change', function () {
+  document.body.classList.toggle('light-theme');
+  document.body.classList.toggle('dark-theme');
+});
