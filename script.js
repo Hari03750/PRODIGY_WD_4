54cf8106ec40a2110c9666f3632ed9c8e99ef928
Hari03750 <hariharan2004b@gmail.com>
2025-05-30 14:19:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,3 @@ document.getElementById('themeToggle').addEventListener('change', function () {
   document.body.classList.toggle('light-theme');
   document.body.classList.toggle('dark-theme');
 });
-document.querySelectorAll("section, .project-card, .card, .contact-card").forEach((el) => {
-  el.classList.add("reveal");
-  observer.observe(el);
-});
